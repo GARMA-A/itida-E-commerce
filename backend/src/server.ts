8b1b45b6d4b7ef910d9060db816cd79d7896baca
Girgis <girgisemad74@gmail.com>
2025-08-20 15:22:09 +0300

    add DB connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import rootRouter from "./routes/rootRouter.ts";
+import mongoose from "mongoose";
 
 
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-
-
+app.use(express.json());
 app.use("/", rootRouter);
 
-app.listen(PORT, () => {
-	console.log("Server is running on http://localhost:3000");
-});
+
+mongoose.connect(process.env.MONGO_URI as string)
+	.then(() => {
+		console.log("✅ MongoDB connected");
+		app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+	})
+	.catch(err => console.error("❌ DB connection error:", err));
