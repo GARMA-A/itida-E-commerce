3359699cc92bedeb2ef21d8f760acdf5de82eb97
zomar26 <omarehab26102004@gmail.com>
2025-08-25 11:52:15 +0300

    user v9

--- backend/src/routes/rootRouter.ts
+++ backend/src/routes/rootRouter.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import RootController from "../controllers/rootController";
+
 const router = express.Router();
-// Root route
+
 router.get("/", RootController);
+
 export default router;
