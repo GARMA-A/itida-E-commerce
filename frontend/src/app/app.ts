80b4921589b3d15ff3ea9dc72a3f8607b86d8489
zomar26 <omarehab26102004@gmail.com>
2025-08-27 00:32:07 +0300

    contactV1.0

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactFormComponent } from './components/contact-form/contact-form';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , ContactFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
