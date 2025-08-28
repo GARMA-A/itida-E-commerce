0b41c38e58af4ea9edd298b6ba7e7eb1efa92ce6
Girgis <girgisemad74@gmail.com>
2025-08-28 09:06:39 +0300

    add user Schema and CRUD

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import productRoutes from "./routes/productRoutes";
 import cartRoutes from "./routes/cartRoutes";
 import orderRoutes from "./routes/orderRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 app.use(
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
