ebde50f58b0e554d505340171bee9f0b754f8b15
zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2025-08-28 05:21:39 +0300

    home,product,items,header,footer done

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { ProductModal } from "./components/product-modal/product-modal";
+import { ItemsComponent } from "./components/items/items";
+import { Home } from "./pages/home/home";
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./shared/header/header";
+import { Footer } from "./shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
